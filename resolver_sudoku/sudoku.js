6fc1ca7e8917e30893ead87a795a31a8b7a6123c
gquesnot <32057757+gquesnot@users.noreply.github.com>
2019-10-25 00:53:25 +0200

    sudoku

--- resolver_sudoku/sudoku.js
+++ resolver_sudoku/sudoku.js
@@ -59,30 +59,58 @@ function showInTab(tab)
 }
 
 
+function clean_sudoku()
+{
+	var tmp = document.querySelectorAll("td");
+	var i= 0;
+	var tmp_class;
+	while (i < tmp.length)
+	{	
+		if (tmp[i].classList)
+			tmp[i].classList= null;
+		tmp[i].innerHTML = "<br>";
+		i++;
+	}	
+}
+
 
 function sudoku()
 {
 	var index = 0;
-	var tab2 = parseTab();
+	let tab2 = parseTab();
+	var coor={
+		'x':0,
+		'y':0,
+	};
 
-	console.log(tab2);
 
-	resolve(tab2, index);
-	showInTab(tab2);
-
+	var tab3 = tab2;
+	
+	if (is_valid(tab2))
+	{	
+		resolve(tab2, index);
+		resolve_reverse(tab3, 80);
+		if (!sudoku_diff(tab2, tab3))
+			alert("sudoku non valide: plusieur solution");
+		if(!is_valid(tab2))
+			alert("erreur resultat non valide");
+		showInTab(tab2);
+	}
+	else
+		alert("Sudoku invalide");
 }
 
 
 
-function show_sudoku(tab)
+/*function show_sudoku(tab)
 {
 var i = 0;
 var j= 0;
 document.write("<br/><br/>")
-while (j < 9)
+while (j < tab.length)
 {
 	i = 0;
-	while (i < 9)
+	while (i < tab[j].length)
 	{
 		document.write(tab[j][i]+" ");
 		i++;
@@ -90,65 +118,80 @@ while (j < 9)
 	document.write("<br/>");
 	j++;
 }
+}*/
+
+
+function check_square(number, y, x, tab)
+{
+	var i = 0;
+	var j = 0;
+	while (j <  3)
+	{
+		while (i < 3)
+		{
+			if(tab[(y - y%3)+j][(x - x%3)+i]== number &&  (y-y%3) + j != y && (x-x%3) + i!= x)
+				return (false);
+			i++;
+		}
+		i = 0;
+		j++;
+
+	}
+	return(true);
 }
+
+
+
+
 
 function is_safe(number, y, x, tab)
 {
 	var i = 0;
+
+
 	while (i < 9)
 	{
-		if (tab[y][i] == number)
+		if (tab[y][i] == number && i != x)
 			return (false);
 		i++;
 	}
 	i = 0;
 	while (i <  9)
 	{
-		if (tab[i][x] == number)
+		if (tab[i][x] == number && y != i)
 			return (false);
 		i++;
 	}
+	if (!check_square(number, y, x, tab))
+		return (false);
 	return(true);
 }
 
-function find_next(y, x, tab)
-{
-	var i = 0;
-	var j = 0;
-
-	while (j <  9)
-	{
-		while (i < 9)
-		{
-			if (tab[j][i] == '.')
-				return ((j*9)+i);
-			i++;
-		}
-		i = 0;
-		j++;
-	}
-	return ((j*9)+i +1);
-}
 
 
 function resolve(tab, index)
 {
 	var i = 1;
 
-	index = find_next(Math.floor(index/9), index%9, tab) ;
+	
+	if (index <= 80 && tab[Math.floor(index/9)][index%9] != '.')
+		{
+			if (resolve(tab, index+1))
+				return (true);
+			else
+				return (false);
+	}
 	if (index >= 81)
 		return (true);
 	while (i <= 9)
 	{
 		if (is_safe(i, Math.floor(index/9), index%9, tab))
-		{  
-			show_sudoku(tab);
+		{	
+			
 			tab[Math.floor(index/9)][index%9]=i;
-			if (resolve(tab, index + 1))
+			if (resolve(tab, index+1))
 				return  (true);
 		}
-
-
 		i++;
 	}
 	tab[Math.floor(index/9)][index%9] = '.';
@@ -156,4 +199,74 @@ function resolve(tab, index)
 }
 
 
+function resolve_reverse(tab, index)
+{
+	var i = 9;
+
+
+	if (index > 0 && tab[Math.floor(index/9)][index%9] != '.')
+	{
+		if (resolve_reverse(tab, index-1))
+			return (true);
+		else
+			return (false);
+	}
+	if (index == 0)
+		return (true);
+	while (i >= 0)
+	{
+		if (is_safe(i, Math.floor(index/9), index%9, tab))
+		{
+			tab[Math.floor(index/9)][index%9]=i;
+			if (resolve_reverse(tab, index - 1))
+				return  (true);
+		}
+
+
+		i--;
+	}
+	tab[Math.floor(index/9)][index%9] = '.';
+	return (false);
+}
+
+
+
+function is_valid(tab)
+{
+	var i = 0;
+	var j = 0;
+
+	while (j < 9)
+	{
+		while (i < 9)
+		{
+			if (tab[j][i] != '.')
+				if (!is_safe(tab[j][i], j , i , tab))
+					return (false);
+			i++;
+		}
+		i = 0;
+		j++;
+	}
+	return (true);
+}
+
+
+function sudoku_diff(tab, tab2)
+{
+	var i = 0;
+	var j = 0;
+	while (j < 9)
+	{
+		while (i < 9)
+		{
+			if (tab[j][i] != tab2[j][i])
+				return(false)
+			i++;
+		}
+		i = 0;
+		j++;
+	}
+	return (true);
+}
 
