8bcc81c4fba4fe8be182597400060e8899b35bd9
gquesnot <gaqu1994@gmail.com>
2019-12-23 14:29:23 +0100

    a

--- slider/js/script.js
+++ slider/js/script.js
@@ -3,16 +3,9 @@
 
 
 $(function(){
-	$("#enfant > img").draggable({ disabled: true });
+	$("#enfant > img").on("dragstart", function(){},false);	
 	var i = 0;
-	$('#enfant > img').click(function(){
-				$('#enfant > img').each(function(){
-					$(this).removeClass('opacity');
-				});
-				$(this).addClass('opacity');
-				$('#drop').html("<img src=\""+$(this).attr('src')+"\">");
-			});
-
+	$('#enfant > img').on("mousedown", click_pic);
 
 	const slider = document.querySelector('#enfant');
 let isDown = false;
@@ -21,6 +14,7 @@ let scrollTop;
 
 slider.addEventListener('mousedown', (e) => {
   isDown = true;
+  $('#enfant > img').off('click', click_pic);
   slider.classList.add('active');
   startY = e.pageY - slider.offsetTop;
   scrollTop = slider.scrollTop;
@@ -28,19 +22,48 @@ slider.addEventListener('mousedown', (e) => {
 slider.addEventListener('mouseleave', () => {
   isDown = false;
   slider.classList.remove('active');
+  // $('#enfant > img').on('click', click_pic);
 });
 slider.addEventListener('mouseup', () => {
   isDown = false;
   slider.classList.remove('active');
+  $('#enfant > img').on('click', click_pic);
 });
 slider.addEventListener('mousemove', (e) => {
+  
   if(!isDown) return;
   e.preventDefault();
   const y = e.pageY - slider.offsetTop;
-  const walk = (y - startY) * 2; //scroll-fast
+  const walk = (y - startY) * 3; //scroll-fast
   slider.scrollTop = scrollTop - walk;
 });
 
+	function click_pic()
+	{
+		$('#enfant > img').off("mousedown", click_pic);
+		$('#enfant > img').on("mouseleave", click3_pic);
+		$('#enfant > img').on("mouseup", click2_pic)
+	}
+
+	function click2_pic()
+	{
+		$('#enfant > img').each(function(){
+			$(this).removeClass('opacity');
+		});
+		$(this).addClass('opacity');
+		$('#drop').html("<img src=\""+$(this).attr('src')+"\">");	
+		$('#enfant > img').off("mouseup", click2_pic);
+		$('#enfant > img').off("mouseleave", click3_pic);
+		$('#enfant > img').on("mousedown", click_pic);
+	}
+
+	function click3_pic()
+	{
+		console.log('move');
+		$('#enfant > img').off("mouseup", click2_pic);
+		$('#enfant > img').off("mouseleave", click3_pic);
+		$('#enfant > img').on("mousedown", click_pic);
+	}
 
 
 });
